32a3e6422819917320bcb503345a4bd3c839f00e
RanDY <baby6675525@gmail.com>
2019-06-05 22:52:14 +0800

    json gson

--- app/src/main/java/com/tom/atm/TransActivity.java
+++ app/src/main/java/com/tom/atm/TransActivity.java
@@ -1,11 +1,23 @@
 package com.tom.atm;
 
 import android.os.AsyncTask;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 
 import org.jetbrains.annotations.NotNull;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -13,6 +25,8 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
 import okhttp3.Call;
 import okhttp3.Callback;
@@ -23,11 +37,16 @@ import okhttp3.Response;
 public class TransActivity extends AppCompatActivity {
 
     private static final String TAG =TransActivity.class.getSimpleName() ;
+    private RecyclerView recyclerView;
+    private List<Transaction> transactions;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_trans);
+        recyclerView = findViewById(R.id.recycler);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
         //new TransTask().execute("http://atm201605.appspot.com/h");
         OkHttpClient client=new OkHttpClient();
         Request request=new Request.Builder()
@@ -41,9 +60,77 @@ public class TransActivity extends AppCompatActivity {
 
             @Override
             public void onResponse(@NotNull Call call, @NotNull Response response) throws IOException {
-                Log.d(TAG, "onResponse: "+response.body().string());
+                final String json=response.body().string();
+                Log.d(TAG, "onResponse: "+json);
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        //parseJSON(json);
+                        parseGSON(json);
+                    }
+                });
             }
         });
+    }
+
+    private void parseGSON(String json) {
+        Gson gson=new Gson();
+        transactions=gson.fromJson(json,new TypeToken<ArrayList<Transaction>>(){}.getType());
+        TransAdapter adapter=new TransAdapter();
+        recyclerView.setAdapter(adapter);
+    }
+
+    private void parseJSON(String json) {
+        transactions = new ArrayList<>();
+        try {
+            JSONArray array=new JSONArray(json);
+            for (int i = 0; i < array.length(); i++) {
+                JSONObject object=array.getJSONObject(i);
+                transactions.add(new Transaction(object));
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        TransAdapter adapter=new TransAdapter();
+        recyclerView.setAdapter(adapter);
+    }
+
+    public class TransAdapter extends RecyclerView.Adapter<TransAdapter.TransHolder> {
+        @NonNull
+        @Override
+        public TransHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+            View view=getLayoutInflater().inflate(R.layout.item_transaction,viewGroup,false);
+            return new TransHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull TransHolder transHolder, int i) {
+            Transaction tran=transactions.get(i);
+            transHolder.bindTo(tran);
+        }
+
+        @Override
+        public int getItemCount() {
+            return transactions.size();
+        }
+
+        public class TransHolder extends RecyclerView.ViewHolder{
+            TextView dateText;
+            TextView amountText;
+            TextView typeText;
+            public TransHolder(@NonNull View itemView) {
+                super(itemView);
+                dateText=itemView.findViewById(R.id.item_date);
+                amountText=itemView.findViewById(R.id.item_amount);
+                typeText=itemView.findViewById(R.id.item_type);
+            }
+
+            public void bindTo(Transaction tran) {
+                dateText.setText(tran.getDate());
+                amountText.setText(String.valueOf(tran.getAccount()));
+                typeText.setText(String.valueOf(tran.getType()));
+            }
+        }
     }
 
     public class TransTask extends AsyncTask<String,Void,String>{
